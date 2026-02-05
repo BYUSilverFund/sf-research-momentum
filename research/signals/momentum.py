107c8a5e0079030332728cc1e45209b7fc62efa2
Grant Rich <grantrich@Grants-MacBook-Air-534.local>
2026-02-05 12:35:37 -0700

    memo signals

--- research/signals/momentum.py
+++ research/signals/momentum.py
@@ -2,10 +2,10 @@ import polars as pl
 
 def momentum() -> pl.Expr:
     return (
-        pl.col('return')
+        pl.col("return")
         .log1p()
         .rolling_sum(230)
         .shift(21)
-        .over('barrid')
-        .alias('momentum')
+        .over("barrid")
+        .alias("momentum")
     )
